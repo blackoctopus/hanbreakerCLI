0621cee14c0a73e0794b000a610b8ddc8269938f
Philip Mozolak <philip.mozolak@icloud.com>
2013-12-08 20:10:52 -0500

    Cleaned and added tags

--- Breaker/hbrake_custom.sh
+++ Breaker/hbrake_custom.sh
@@ -1,5 +1,11 @@
- /Applications/HandBrakeCLI  -i /Users/fcpuser/Desktop/JTDA_OPEN.mov  -o /Users/fcpuser/Desktop/houtput.mp4 -b 5000 --vfr -x264 --x264-preset slow
+### Quickie
+
+/Applications/HandBrakeCLI  -i /Users/fcpuser/Desktop/INPUT.mov  -o ~/Desktop/output.mp4 -b 5000 --vfr -x264 --x264-preset slow
           <fast>
-          /Applications/HandBrakeCLI -i /Users/fcpuser/Desktop/ -o /Users/fcpuser/Desktop/OUTPUT.mp4 -m -E copy --audio-copy-mask ac3,dts,dtshd --audio-fallback ffac3 -e x264 -q 20 -x level=4.1:ref=4:b-adapt=2:direct=auto:me=umh:subq=8:rc-lookahead=50:psy-rd=1.0,0.15:deblock=-1,-1:vbv-bufsize=30000:vbv-maxrate=40000:slices=4
-          /Applications/HandBrakeCLI  -i /Users/fcpuser/Desktop/input.mov  -o /Users/fcpuser/Desktop/output.mp4 -m -E copy --audio-copy-mask ac3,dts,dtshd --audio-fallback ffac3 -e x264 -q 20 -x level=4.1:ref=4:b-adapt=2:direct=auto:me=umh:subq=8:rc-lookahead=50:psy-rd=1.0,0.15:deblock=-1,-1:vbv-bufsize=3000:vbv-maxrate=50000:slices=4
+          
+          
+/Applications/HandBrakeCLI -i ~/Desktop/ -o /Users/fcpuser/Desktop/OUTPUT.mp4 -m -E copy --audio-copy-mask ac3,dts,dtshd --audio-fallback ffac3 -e x264 -q 20 -x level=4.1:ref=4:b-adapt=2:direct=auto:me=umh:subq=8:rc-lookahead=50:psy-rd=1.0,0.15:deblock=-1,-1:vbv-bufsize=30000:vbv-maxrate=40000:slices=4
+          
+          
+/Applications/HandBrakeCLI  -i ~/Desktop/input.mov  -o /output.mp4 -m -E copy --audio-copy-mask ac3,dts,dtshd --audio-fallback ffac3 -e x264 -q 20 -x level=4.1:ref=4:b-adapt=2:direct=auto:me=umh:subq=8:rc-lookahead=50:psy-rd=1.0,0.15:deblock=-1,-1:vbv-bufsize=3000:vbv-maxrate=50000:slices=4
           
