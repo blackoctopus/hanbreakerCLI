1ff154ace3afe0ef4a6dd431c509f9e8a9eaffd7
Philip Mozolak <philip.mozolak@icloud.com>
2015-03-06 13:01:22 -0500

    Test file

--- Breaker/hbrake_custom.sh
+++ Breaker/hbrake_custom.sh
@@ -9,3 +9,9 @@
 ## Quality max bitrate 5000
 /Applications/HandBrakeCLI  -i ~/Desktop/input.mov  -o /output.mp4 -m -E copy --audio-copy-mask ac3,dts,dtshd --audio-fallback ffac3 -e x264 -q 20 -x level=4.1:ref=4:b-adapt=2:direct=auto:me=umh:subq=8:rc-lookahead=50:psy-rd=1.0,0.15:deblock=-1,-1:vbv-bufsize=3000:vbv-maxrate=50000:slices=4
           
+
+
+##Test file
+
+/Applications/HandBrakeCLI  -i ~/Desktop/lspeed1.mov  -o ~/Desktop/output.mp4 -b 3000 --vfr -x264 --x264-preset slow
+<fast>
